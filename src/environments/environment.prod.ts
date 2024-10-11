39f41d5a8ea7eafe1cb144bfbfd30a74313214e6
Sayyid Sajad <sayyidsajada4s@gmail.com>
2024-10-11 11:20:25 +0530

    latest

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: process.env.API_URL,
-  razorKey: process.env.RAZORPAY_KEY,
-  googleProvider: process.env.GOOGLE_PROVIDER,
-  gCloudOrigin: process.env.GCLOUD_ORIGIN,
-  googleMapsApi: process.env.GOOGLE_MAPS_API_KEY,
+  // apiUrl: "http://lab.foxfennecs.com:8080/yohire",
+  apiUrl: 'https://www.20heads.com',
+  razorKey: 'rzp_live_o356NLpLTlPQjD',
+  googleProvider:
+    '473077239498-skrqjvo0msei4oc7pp4q37qojs0dsujc.apps.googleusercontent.com',
+  gCloudOrigin:
+    'https://firebasestorage.googleapis.com/v0/b/yohire-a7f13.appspot.com',
+  googleMapsApi:
+    'https://maps.googleapis.com/maps/api/js?key=AIzaSyBCeQj1xv23zcjK1VfUKMdcJD8-k1upzuM&loading=async&libraries=places&callback=initMap',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: false,
-  googleProvider: process.env.GOOGLE_PROVIDER_TEST,
-  apiUrl: process.env.API_URL_TEST,
-  clientSecret: process.env.CLIENT_SECRET,
-  apiVersion: process.env.API_VERSION,
-  razorKey: process.env.RAZOR_TEST_KEY,
-  gCloudOrigin: process.env.GCLOUD_ORIGIN_TEST,
-  googleMapsApi: process.env.GOOGLE_MAPS_API_KEY,
+  googleProvider:
+    '473077239498-skrqjvo0msei4oc7pp4q37qojs0dsujc.apps.googleusercontent.com',
+  apiUrl: 'http://localhost:3000',
+  clientSecret:'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',
+  apiVersion: '/api/v1/',
+  razorKey: 'rzp_test_RwYMZBF65TFGRT',
+  gCloudOrigin:
+    'https://firebasestorage.googleapis.com/v0/b/yohire-a7f13.appspot.com',
+  // apiUrl: 'https://api.yohire.in',
+  // apiUrl: "http://lab.foxfennecs.com:8080/yohire",
+  // apiUrl: "http://192.168.1.100:8080/yohire",
+  // apiUrl: "http://192.168.1.100:8080/yohire-dev",
 };
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
